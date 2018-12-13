47aeebab8d760a8bf71f987d614ecf0a408c2184
Kevin Chen <kchen01@stuy.edu>
2018-12-13 14:44:27 -0500

    node's comments are now added

--- Node.java
+++ Node.java
@@ -1,30 +1,51 @@
 class Node{
+  // stores numbers
  private int data;
+ // can connects to other nodes
  private Node next,prev;
+
+ //node constructor with 3 elements
  public Node(Node a,int b,Node c) {
    prev = a;
    data = b;
    next = c;
  }
+ //node contrusctor with a beginning
  public Node(int b,Node c) {
    data = b;
    next = c;
  }
+ //node constructor with an end
  public Node(Node a,int b) {
    prev = a;
    data = b;
  }
- public int get() {
+ // returns data
+ public int getData() {
    return data;
  }
- public void set(int i) {
+ // sets next as something else
+ public void setNext(Node other) {
+   next = other;
+ }
+ // sets previous as something
+ public void setPrev(Node other) {
+   prev = other;
+ }
+ // sets data as a different value
+ public void setData(int i) {
    data = i;
  }
+ // returns the next node
  public Node next() {
    return next;
  }
+ //returns the previous node
  public Node prev() {
    return prev;
  }
-
+ //returns the data in string form
+ public String toString() {
+   return "" + data;
+ }
 }
