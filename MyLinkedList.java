48fb1d65387fa1a5eea494b4db26dde1d93f4b39
Kevin Chen <kchen01@stuy.edu>
2018-12-14 14:51:29 -0500

    added classwork

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,14 +2,26 @@ class MyLinkedList{
  private int length;
  private Node start,end;
  public MyLinkedList() {
-
+   length = 0;
  }
+public void clear() {
+  start = null;
+  end = null;
+  length = 0;
+}
  public int size() {
    return length;
  }
  public boolean add(int value) {
+   Node input = new Node(end,value);
+   if (length == 0) {
+     end = input;
+     start = input;
+   }
    return true;
  }
+
+
  public String toString() {
    return "";
  }
